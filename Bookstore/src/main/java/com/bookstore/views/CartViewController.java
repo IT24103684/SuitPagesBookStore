fc76dabef1788a9b0358b52a25740b4e9c393dbd
IT24103306 <it24103306@my.sliit.lk>
2025-05-18 16:44:44 +0530

    imported with CartService java

--- Bookstore/src/main/java/com/bookstore/views/CartViewController.java
+++ Bookstore/src/main/java/com/bookstore/views/CartViewController.java
@@ -1,7 +1,6 @@
 package com.bookstore.views;
 
 import com.bookstore.dtos.CartDTO;
-import com.bookstore.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
