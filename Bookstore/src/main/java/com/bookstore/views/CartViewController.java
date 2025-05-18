eccc9578a8c96251d30b34c0f49b3a37d7c7f6ec
IT24103306 <it24103306@my.sliit.lk>
2025-05-18 16:49:11 +0530

    modify with import CartService java

--- Bookstore/src/main/java/com/bookstore/views/CartViewController.java
+++ Bookstore/src/main/java/com/bookstore/views/CartViewController.java
@@ -1,6 +1,7 @@
 package com.bookstore.views;
 
 import com.bookstore.dtos.CartDTO;
+import com.bookstore.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
