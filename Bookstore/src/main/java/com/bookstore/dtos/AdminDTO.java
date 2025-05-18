ed276f7883de2cdd94f2a3e51af7836065dffc08
UdayangaGWA <It24103260@my.sliit.lk>
2025-05-18 11:44:56 +0530

    I created AdminDTO java

---
+++ Bookstore/src/main/java/com/bookstore/dtos/AdminDTO.java
@@ -0,0 +1,83 @@
+package com.bookstore.dtos;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class AdminDTO {
+    private String id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "NIC number is required")
+    private String nicNumber;
+
+    // Default constructor
+    public AdminDTO() {
+    }
+
+    // Constructor without id for creating new admins
+    public AdminDTO(String name, String email, String password, String nicNumber) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.nicNumber = nicNumber;
+    }
+
+    // Full constructor
+    public AdminDTO(String id, String name, String email, String password, String nicNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.nicNumber = nicNumber;
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNicNumber() {
+        return nicNumber;
+    }
+
+    public void setNicNumber(String nicNumber) {
+        this.nicNumber = nicNumber;
+    }
+}
